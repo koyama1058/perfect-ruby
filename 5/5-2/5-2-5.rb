24260cdbc766b4a2325085508e0c2e6099177cd9
koyama1058 <koyama1058tk@gmail.com>
2021-01-11 11:17:40 +0900

    5-2-5完了

---
+++ 5/5-2/5-2-5.rb
@@ -0,0 +1,15 @@
+# 繰り返し処理
+
+# 文字ごとの繰り返し
+'ブフウ'.each_char {|c| print "#{c}-"}
+
+# バイトごとの繰り返し
+'る'.each_byte do |byte|
+  puts byte
+end
+
+# 行ごとの繰り返し
+"Alice\nBob\nCharlie".each_line do |line|
+  puts line
+end
+
