891331da4cb3f9c04a03b6afc994adb9be84a5be
koyama1058 <koyama1058tk@gmail.com>
2021-01-25 10:41:05 +0900

    5-7-3完了

---
+++ 5/5-7/5-7-3.rb
@@ -0,0 +1,21 @@
+# ファイル名の変更
+# File.rename 'foo', 'dest'
+
+# ファイルの移動
+# File.rename 'dest', '5-6'
+
+# ファイルの削除
+# File.unlink 'counter'
+
+# ファイルのシンボリックリンクを作成する
+# File.symlink 'origin', 'sym'
+
+# ハードリンクを作成する
+# File.link 'origin', 'link'
+
+# ファイルモードの指定（モードは8進数で指定）
+# File.chmod 0600, 'fname'
+
+# オーナー、グループの変更
+# File.chown 216, 216, 'fname'
+
