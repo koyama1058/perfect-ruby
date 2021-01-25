5073445e2cecdf9fb2a689bc7abb98dd48e859cb
koyama1058 <koyama1058tk@gmail.com>
2021-01-25 11:41:37 +0900

    6-1完了

---
+++ 6/6-1/6-1.rb
@@ -0,0 +1,11 @@
+# Classクラスはどの様なクラスを承継しているかを確認するためには以下の様に記述する
+p Class.ancestors
+#  => [Class, Module, Object, Kernel, BasicObject]
+# Classクラスの親クラスはModuleであることがわかる。
+
+# 継承したメソッドを除いた特定のクラスで定義されているメソッド名を調べるためにはinstance_methodsにfalseを渡すと確認することができる
+p Class.instance_methods(false)
+# => [:allocate, :superclass, :new]
+# allocateはinitializeメソッドを実行せずにインスタンスを作るメソッド
+# newはinitializeを実行してインスタンスを作るメソッド
+# superclassはクラスの親クラスを返るメソッド
